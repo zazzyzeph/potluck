c3c706fe7585a753fb8b1cc598b12bdd3703ee61
zazzyzeph <zephyr.prusinski@gmail.com>
2015-12-23 17:16:33 -0500

    grunt stuff

---
+++ gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+  grunt.initConfig ({
+    sass: {
+      dist: {
+        files: {
+          'public/css/style.css' : 'sass/app.scss'
+        },
+      },
+    },
+    watch: {
+      sass: {
+        files: ['sass/**/*.scss'],
+        tasks: ['sass'],
+        options: {
+          livereload : true
+        },
+      },
+      jade: {
+        files: ['views/**/*.jade'],
+        options: {
+          livereload : true
+        },
+      },
+      js: {
+        files: ['public/js/**/*.js'],
+        options: {
+          livereload : true
+        },
+      },
+    },
+  });
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['watch']);
+};
+
+
