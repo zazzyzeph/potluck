0f6c808c25498d9c7ea48a30bd9a128df33e9581
Zephyr Prusinski <zprusinski@flightpath.com>
2016-04-11 22:05:15 -0400

    upDATE

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,6 @@ var potluckValidation = angular.module('potluckValidation', ['ngAnimate']);
 
 // create angular controller
 indexValidation.controller('mainController', function($scope) {
-
 	// function to submit the form after all validation has occurred			
 	$scope.submitForm = function() {
 		// check to make sure the form is completely valid
